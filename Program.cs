ae3cf49701e195045bbd93b84df565c03323522f
EgeniiM <106548205+EgeniiM@users.noreply.github.com>
2022-06-02 23:04:35 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int allPeople;
+            int onePeopleWait = 10;
+            int allwaiting;
+            int hours;
+            int minutes;
+            string input;
+
+            Console.WriteLine("Вы заходите в поликлинику и видите огромную очередь из старушек.");
+            Console.WriteLine("введите количество старушек:");
+            input = Console.ReadLine();
+            allPeople = Convert.ToInt32(input);
+            hours = (allPeople * onePeopleWait) / 60;
+            minutes = (allPeople * onePeopleWait) % 60;
+            Console.WriteLine($"Надо ждать {hours} часов и {minutes} минут.");
+        }
+    }
+}
